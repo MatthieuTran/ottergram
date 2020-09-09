a470769b92ac65a3896e046dccb8cee8fecf6035
Matthieu Tran <matthieuktran@gmail.com>
2020-09-09 12:06:47 -0700

    Add hw

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails() {
+    'use strict';
+
+    var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src', 'img/otter4.jpg');
+}
